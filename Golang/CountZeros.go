cb254163e103b8a7932eecad08392065083e72a1
Sanket Kumar Choudhary <sanketkumarchoudhary@gmail.com>
2022-09-27 04:39:34 +0530

    Create CountZeros.go

---
+++ Golang/CountZeros.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main() {
+	a := [...]int{0, 1, 0, 1, 0, 1}
+	var count=0;
+	for i:=0;i<=len(a)-1;i++{
+		if(a[i]==0){
+		count =count+1
+		}
+	}
+	fmt.Println("Count; ",count)
+
+}
