48cb312fc11f202f0fb6a5b123e87258e98e800b
Sanket Kumar Choudhary <sanketkumarchoudhary@gmail.com>
2022-10-05 20:32:44 +0530

    Create Exam19.py

---
+++ Exam19.py
@@ -0,0 +1,15 @@
+import sys
+def solution(arr,src,dest):
+    
+    return
+
+if __name__=="__main__":
+    testcases=1#int(input())
+    for i in range(0,testcases):
+        size=23#input()
+        ta=#input().split(" ")
+        arr=[]
+        for i in ta:
+            arr.append(int(i))
+        (src,dest)=(int(z) for z in input().split(" "))
+        print(solution(arr,src,dest))
